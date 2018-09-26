2fb45b2d1075d75359f62de22998fe0d4e7acb6c
Elias Keven <eliaskeven2@gmail.com>
2018-09-26 16:47:20 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,22 @@
+const express = require("express")
+
+const app = express()
+
+app.get("/random/:min/:max", function (req, res) {
+	let min = parseInt(req.params.min)
+	let max = parseInt(req.params.max)
+
+	if (isNaN(min) || isNaN(max)) {
+		res.status(400)
+		res.json({ error: "Bad request." })
+		return;
+	}
+	const result = Math.round( (Math.random() * (max - min)) + min)
+
+	res.json({ result: result })
+})
+
+
+app.listen(3000, function () {
+	console.log("App started on port 3000.")
+})
